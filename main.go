09b70cce047c2395ee0a41ced1dda709e0fcf118
Nikita_Gusev <dr.after@mail.ru>
2025-04-13 15:20:37 +0500

    added router

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	"log"
 
+	"github.com/Ma3au88/goSimpleBank/api"
+	db "github.com/Ma3au88/goSimpleBank/db/sqlc"
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	if err = server.Start(serverAddress); err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
